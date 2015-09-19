fc9487778a379e7147320274789b7567fb0818d8
Lecks <leckschester@hotmail.com>
2015-09-19 14:33:40 +1000

    make make.py work.

--- tools/make.py
+++ tools/make.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
 
 # make.py
@@ -30,7 +30,7 @@
 
 ###############################################################################
 
-__version__ = "0.7"
+__version__ = "0.4"
 
 import sys
 
@@ -49,7 +49,6 @@ import configparser
 import json
 import traceback
 import time
-import timeit
 import re
 
 from tempfile import mkstemp
@@ -59,7 +58,7 @@ if sys.platform == "win32":
 
 ######## GLOBALS #########
 project = "@ace"
-project_version = "3.0.0"
+ACE_VERSION = "3.0.0"
 arma3tools_path = ""
 work_drive = ""
 module_root = ""
@@ -116,19 +115,6 @@ def  get_directory_hash(directory):
     retVal = directory_hash.hexdigest()
     #print_yellow("Hash Value for {} is {}".format(directory,retVal))
     return directory_hash.hexdigest()
-
-def Fract_Sec(s):
-    temp = float()
-    temp = float(s) / (60*60*24)
-    d = int(temp)
-    temp = (temp - d) * 24
-    h = int(temp)
-    temp = (temp - h) * 60
-    m = int(temp)
-    temp = (temp - m) * 60
-    sec = temp
-    return d,h,m,sec
-    #endef Fract_Sec
 
 # Copyright (c) André Burgaud
 # http://www.burgaud.com/bring-colors-to-the-windows-console-with-python/
@@ -386,7 +372,6 @@ def copy_optionals_for_building(mod,pbos):
             if (os.path.isfile(sig_path)):
                 #print("Moving {} for processing.".format(sig_path))
                 shutil.move(sig_path, os.path.join(release_dir, project, "addons", sigFile_name))
-
     except:
         print_error("Error in moving")
         raise
@@ -449,9 +434,6 @@ def cleanup_optionals(mod):
                 print_error("{} already exists".format(file_name))
                 continue
             shutil.rmtree(destination)
-
-    except FileNotFoundError:
-        print_yellow("{} file not found".format(file_name))
 
     except:
         print_error("Cleaning Optionals Failed")
@@ -559,9 +541,9 @@ def addon_restore(modulePath):
     return True
 
 
-def get_project_version():
-    global project_version
-    versionStamp = project_version
+def get_ace_version():
+    global ACE_VERSION
+    versionStamp = ACE_VERSION
     #do the magic based on https://github.com/acemod/ACE3/issues/806#issuecomment-95639048
 
     try:
@@ -586,16 +568,16 @@ def get_project_version():
             raise FileNotFoundError("File Not Found: {}".format(scriptModPath))
 
     except Exception as e:
-        print_error("Get_project_version error: {}".format(e))
+        print_error("Get_Ace_Version error: {}".format(e))
         print_error("Check the integrity of the file: {}".format(scriptModPath))
-        versionStamp = project_version
+        versionStamp = ACE_VERSION
         print_error("Resetting to the default version stamp: {}".format(versionStamp))
         input("Press Enter to continue...")
         print("Resuming build...")
 
     print_yellow("{} VERSION set to {}".format(project.lstrip("@").upper(),versionStamp))
-    project_version = versionStamp
-    return project_version
+    ACE_VERSION = versionStamp
+    return ACE_VERSION
 
 
 def replace_file(filePath, oldSubstring, newSubstring):
@@ -613,7 +595,7 @@ def replace_file(filePath, oldSubstring, newSubstring):
 
 
 def set_version_in_files():
-    newVersion = project_version # MAJOR.MINOR.PATCH.BUILD
+    newVersion = ACE_VERSION # MAJOR.MINOR.PATCH.BUILD
     newVersionShort = newVersion[:-2] # MAJOR.MINOR.PATCH
 
     # Regex patterns
@@ -695,7 +677,7 @@ def restore_version_files():
 def get_private_keyname(commitID,module="main"):
     global pbo_name_prefix
 
-    aceVersion = get_project_version()
+    aceVersion = get_ace_version()
     keyName = str("{prefix}{version}-{commit_id}".format(prefix=pbo_name_prefix,version=aceVersion,commit_id=commitID))
     return keyName
 
@@ -772,7 +754,7 @@ def main(argv):
     """Build an Arma addon suite in a directory from rules in a make.cfg file."""
     print_blue("\nmake.py for Arma, modified for Advanced Combat Environment v{}".format(__version__))
 
-    global project_version
+    global ACE_VERSION
     global arma3tools_path
     global work_drive
     global module_root
@@ -858,7 +840,7 @@ See the make.cfg file for additional build options.
         argv.remove("release")
     else:
         make_release_zip = False
-        release_version = project_version
+        release_version = ACE_VERSION
 
     if "target" in argv:
         make_target = argv[argv.index("target") + 1]
@@ -881,7 +863,7 @@ See the make.cfg file for additional build options.
         check_external = True
     else:
         check_external = False
-
+    
     if "version" in argv:
         argv.remove("version")
         version_update = True
@@ -915,7 +897,7 @@ See the make.cfg file for additional build options.
 
         # Project prefix (folder path)
         prefix = cfg.get(make_target, "prefix", fallback="")
-
+        
         # Release archive prefix
         zipPrefix = cfg.get(make_target, "zipPrefix", fallback=project.lstrip("@").lower())
 
@@ -951,8 +933,8 @@ See the make.cfg file for additional build options.
         optionals_root = os.path.join(module_root_parent, "optionals")
         extensions_root = os.path.join(module_root_parent, "extensions")
 
-        commit_id = get_commit_ID()
-        key_name = versionStamp = get_private_keyname(commit_id)
+        commit_id = "NOGIT"
+        key_name = versionStamp = "ASORACE1.0"
         print_green ("module_root: {}".format(module_root))
 
         if (os.path.isdir(module_root)):
@@ -1436,8 +1418,5 @@ See the make.cfg file for additional build options.
 
 
 if __name__ == "__main__":
-    start_time = timeit.default_timer()
     main(sys.argv)
-    d,h,m,s = Fract_Sec(timeit.default_timer() - start_time)
-    print("\nTotal Program time elapsed: {0:2}h {1:2}m {2:4.5f}s".format(h,m,s))
 input("Press Enter to continue...")
