58a69fc51609abbbd257df311955e912090adc0d
Lecks <leckschester@hotmail.com>
2015-10-15 17:08:38 +1100

    Revert to medical from 3.1.0

--- addons/medical_menu/ACE_Settings.hpp
+++ addons/medical_menu/ACE_Settings.hpp
@@ -1,3 +1,4 @@
+
 class ACE_Settings {
     class GVAR(allow) {
         displayName = CSTRING(allow);
@@ -5,7 +6,6 @@ class ACE_Settings {
         value = 1;
         typeName = "SCALAR";
         values[] = {ECSTRING(common,Disabled), ECSTRING(common,Enabled), ECSTRING(common,VehiclesOnly)};
-        category = ECSTRING(medical,Category_Medical);
     };
     class GVAR(useMenu) {
         displayName = CSTRING(useMenu);
@@ -14,7 +14,6 @@ class ACE_Settings {
         typeName = "SCALAR";
         values[] = {ECSTRING(common,Disabled), ECSTRING(common,Enabled), ECSTRING(common,VehiclesOnly)};
         isClientSettable = 1;
-        category = ECSTRING(medical,Category_Medical);
     };
     class GVAR(openAfterTreatment) {
         displayName = CSTRING(openAfterTreatment);
@@ -22,6 +21,5 @@ class ACE_Settings {
         typeName = "BOOL";
         value = 1;
         isClientSettable = 1;
-        category = ECSTRING(medical,Category_Medical);
     };
 };
